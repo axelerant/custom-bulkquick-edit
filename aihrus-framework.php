ee7ddd3fe46c8c8a3ffab0cc09195a430dfe89ba
Michael Cannon <mc@aihr.us>
2014-10-25 23:28:44 -0700

    Squashed 'includes/libraries/aihrus-framework/' changes from e31461c..350256a
    350256a Verbiage
    d7f22c1 Version 1.1.6RC2
    
    git-subtree-dir: includes/libraries/aihrus-framework
    git-subtree-split: 350256a5c7e92f0d0470260d61f179826adfceff

--- aihrus-framework.php
+++ aihrus-framework.php
@@ -35,7 +35,7 @@ if ( ! defined( 'AIHR_DIR_LIB' ) ) {
 }
 
 if ( ! defined( 'AIHR_VERSION' ) ) {
-	define( 'AIHR_VERSION', '1.1.6RC1' );
+	define( 'AIHR_VERSION', '1.1.6RC2' );
 }
 
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
