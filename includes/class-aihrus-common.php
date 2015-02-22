e7e0481c1cc1d92afbc5e81c9bac62e0024fd0ff
Michael Cannon <mc@aihr.us>
2015-02-22 17:00:36 +0530

    Squashed 'includes/libraries/aihrus-framework/' changes from 1f3a102..13a49b1
    13a49b1 Update copyright year
    
    git-subtree-dir: includes/libraries/aihrus-framework
    git-subtree-split: 13a49b17b097abedcd178e630d708f48a6df2fce

--- includes/class-aihrus-common.php
+++ includes/class-aihrus-common.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Aihrus Framework
- * Copyright (C) 2014  Michael Cannon
+ * Copyright (C) 2015 Michael Cannon
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
